cb353595ee634ea33821ff7427ecf53e84aad85f
jiho lee <optional.int@kakao.com>
2022-04-12 02:43:43 +0900

    feat: initial commit

---
+++ src/main/java/org/swmaestro/examplek8sapp/ExampleK8sAppApplication.java
@@ -0,0 +1,13 @@
+package org.swmaestro.examplek8sapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleK8sAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleK8sAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/swmaestro/examplek8sapp/ExampleK8sAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swmaestro.examplek8sapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleK8sAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
